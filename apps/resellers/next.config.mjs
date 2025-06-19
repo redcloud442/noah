b1e9c606f44704c1591fdcdbad0204ec831e826a
cloudred <cloudred442@gmail.com>
2025-06-19 16:15:42 +0800

    initial commit

--- apps/resellers/next.config.mjs
+++ apps/resellers/next.config.mjs
@@ -7,7 +7,7 @@ const nextConfig = {
         destination: `${
           process.env.NODE_ENV === "development"
             ? "http://localhost:8080"
-            : "http://localhost:8080"
+            : "https://noah-1v48.onrender.com"
         }/api/v1/:path*`,
       },
     ];
